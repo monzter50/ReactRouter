e3e929744a68922546da95c5bbd2d4c70ae566f9
monzter50 <josemonzter50@gmail.com>
2018-08-28 17:53:34 -0500

    div diagonal

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
-import Layout from '../layout/Layout'
+import Layout from '../layout/Layout';
+import './Home.css';
 class Home extends Component {
   render() {
     return (
-      <h2>Home</h2>
+          <section className="container">
+            <div className="shape-two">
+                <span className="shape-two__text">Using before & after</span>
+
+            </div>
+
+          <div className="shape-two-desc">
+              <div className="shape-container">
+                  hola guapo
+              </div>
+          </div>
+              <div className="shape-three-desc">
+                  <div className="shape-container2">
+                      hola guapo2
+                  </div>
+              </div>
+              <div className="shape-four-desc">
+                  <div className="shape-container3">
+                      hola guapo3
+                  </div>
+              </div>
+          </section>
 
     );
   }
