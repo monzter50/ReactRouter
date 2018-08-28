0d57458f1e4c632c6d385640dd398ad7694bcb12
monzter50 <josemonzter50@gmail.com>
2018-08-28 13:41:17 -0500

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,46 @@
-import React from 'react';
+import React,{Component} from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css'
+class Navbar extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            openNav:false
+        }
+        this.handleOpenNav = this.handleOpenNav.bind(this);
+    }
+    handleOpenNav = () =>{
 
-function Navbar() {
-    return(
-        <div>
-            <h2>Welcome to React Router Tutorial</h2>
+        this.setState({
+            openNav:!this.state.openNav
+        });
+        console.log(this.state.openNav)
+    }
+    render(){
+        return(
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <ul className="navbar-nav mr-auto">
-                    <li><Link to={'/'} className="nav-link"> Home </Link></li>
-                    <li><Link to={'/contact'} className="nav-link">Tickets and Tours</Link></li>
-                </ul>
+                <Link to={'/'} className="navbar-brand"> TEST </Link>
+                <button className="navbar-toggler" type="button" onClick={this.handleOpenNav}>
+                    <div id="menuanim">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </button>
+                <div className={"collapse navbar-collapse "+ (!this.state.openNav ? '':'show')}>
+                    <ul className="navbar-nav mr-auto">
+                        <li><Link to={'/'} className="nav-link"> Home </Link></li>
+                        <li><Link to={'/contact'} className="nav-link">Tickets and Tours</Link></li>
+                        <li><Link to={'/contact'} className="nav-link">Tickets and Tours</Link></li>
+                        <li><Link to={'/contact'} className="nav-link">Tickets and Tours</Link></li>
+                    </ul>
+                </div>
+
             </nav>
-            <hr />
-        </div>
-    )
+        )
+    }
+
 }
 
 export default Navbar;
